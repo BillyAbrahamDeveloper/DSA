b1daf073f122f92dd09c137109036018847b374c
BillyAbrahamDeveloper <contact@billydev.me>
2024-07-10 14:20:09 -0500

    bigOnotation

---
+++ bigOnotaion.js
@@ -0,0 +1,44 @@
+// ---------------------First Approach
+
+// const addUpTo = (n) => {
+//   let total = 0;
+//   for (let i = 1; i <= n; i++) {
+//     total += i;
+//     console.log(total);
+//   }
+
+//   return total;
+// };
+
+// console.log(addUpTo(3));
+
+// --------------------- Second Approach
+
+// const addUpToTwo = (n) => {
+//   return (n * (n + 1)) / 2;
+// };
+
+// console.log(addUpToTwo(9));
+
+// --------------------- Third Approach
+
+// const addUpTo = (n) => {
+//   return (n * (n + 1)) / 2;
+// };
+
+// const t1 = performance.now();
+// addUpTo(1000000002310);
+// const t2 = performance.now();
+
+// console.log(`Time Elapsed: ${t2 - t1} second.`);
+
+///-------/// --------------------- Counting Operation.
+
+// --------------------- Third Approach
+// --------------------- Third Approach
+// --------------------- Third Approach
+// --------------------- Third Approach
+// --------------------- Third Approach
+// --------------------- Third Approach
+// --------------------- Third Approach
+// --------------------- Third Approach
